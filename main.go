95edc13642448c4c5510defbcdaf7d1e5c932ced
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2022-03-07 09:45:55 +0900

    foobar

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package parent
 
 import (
-	"fmt"
 	child "github.com/yamamoto-febc/module-child"
 )
 
-func Foobar() {
-	fmt.Println(child.FooBar() + " from parent")
+func Foobar() string {
+	return child.FooBar() + " from parent"
 }
